3a6b4726d2f989bce92c20f1ad2e7028faba5d73
Ashim Raj <programwithashim@gmail.com>
2025-09-18 04:51:51 +0530

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api";
+const BASE_URL = "https://thinkboard-server.onrender.com/api;
 
 const api = axios.create({
   baseURL: BASE_URL,
